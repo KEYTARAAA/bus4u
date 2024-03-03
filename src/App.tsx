9d876552c779f4befc8455fd53f2239d3ffb8e12
KITARA STEWART <18241476@studentmail.ul.ie>
2024-03-03 18:43:37 +0000

    Add Box spacer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <Center paddingTop={15}>
       <VStack spacing={0}>
         <Notice />
+        <Box bg="black" w="100%" h={100} color="black" />
       </VStack>
     </Center>
   );
